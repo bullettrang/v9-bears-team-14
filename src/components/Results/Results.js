da7940e0a9bafbdcfa753ee3444228724db07abd
Brian <trangb@oregonstate.edu>
2019-06-20 14:26:01 -0700

    configuring Results font size for responsiveness

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -8,9 +8,13 @@ const Results = (props) =>
                 <div className="Results__Wrapper">
                 {meals.map((meal) => 
                     <div className="Results__Card">
-                    <h1  key={meal.idMeal}>{meal.menuTitle}</h1>
-                    <img className="Results__Image"src={meal.menuTitleThumb} alt={meal.menuTitle}/>
-                </div>
+                        <div className="Title__Wrapper">
+                            <h1 className="Title--Heading" key={meal.idMeal}>{meal.menuTitle}</h1>
+                        </div>
+                        <figure className="Results__Image--item">
+                            <img className="Results__Image"src={meal.menuTitleThumb} alt={meal.menuTitle}/>
+                        </figure>
+                    </div>
                 )} 
             </div> 
     </div>
