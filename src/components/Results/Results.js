5525626a7083cf406e60d9652e91fbef72b2bb11
Brian <trangb@oregonstate.edu>
2019-06-22 19:01:17 -0700

    added container class to Results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,24 +1,25 @@
-import React, {useContext} from 'react'
+import React, {useContext} from 'react';
 import './Results.css'
 import useRecipes from '../../hooks/useRecipes';
 import CountryContext from '../../context/country-context'
 import MAP_CONSTANTS from '../Map/MapConstants/MAP_CONSTANTS'
 
-const Results = (props) => {
-const {countrySelected} = useContext(CountryContext)
-const recipes = useRecipes(countrySelected)
-return (
-    <div >
-        {countrySelected.length > 0 &&<h1 className="Results__Title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}         
-        <div className="Results__Wrapper">
-            {recipes.map((meal) => 
-                <div key={meal.idMeal} className="Results__Card">
-                    <h1  >{meal.strMeal}</h1>
-                    <img className="Results__Image"src={meal.strMealThumb} alt={meal.strMeal}/>
+const Results = () => {
+    const {countrySelected} = useContext(CountryContext)
+    const recipes = useRecipes(countrySelected)
+
+    return (
+                <div className="Results__Container">
+                    {countrySelected.length > 0 &&<h1 className="Results__Title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}         
+                            <div className="Results__Wrapper">
+                                {recipes.map((meal) => 
+                                    <div key={meal.idMeal} className="Results__Card">
+                                        <h1  >{meal.strMeal}</h1>
+                                        <img className="Results__Image"src={meal.strMealThumb} alt={meal.strMeal}/>
+                                    </div>
+                                )} 
+                            </div> 
                 </div>
-            )} 
-        </div> 
-    </div>
-    )
+        )
 }
 export default Results;
