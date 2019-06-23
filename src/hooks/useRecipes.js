c01120de5037652743e083fc19cc0388fac4d469
Brian <trangb@oregonstate.edu>
2019-06-22 17:11:50 -0700

    recipe loading context

--- src/hooks/useRecipes.js
+++ src/hooks/useRecipes.js
@@ -1,6 +1,8 @@
-import {useState,useEffect} from 'react';
-import MAP_CONSTANTS from '../components/Map/MapConstants/MAP_CONSTANTS';
+import {useState,useEffect,useContext} from 'react';
 import axios from 'axios';
+import RecipesContext from '../context/recipes-context'
+import MAP_CONSTANTS from '../components/Map/MapConstants/MAP_CONSTANTS';
+
 
 /**
  * @hook - useRecipes - takes in a country argument and returns recipes of that country
@@ -9,20 +11,24 @@ import axios from 'axios';
  */
 const useRecipes= (country)=>{
     const [recipes,setRecipes]= useState([]);
-   
+    const {setLoading} = useContext(RecipesContext);
     
     useEffect(
+        
          ()=>{
-            const {supportedCountries}= MAP_CONSTANTS;          //Need to move into useEffect function to avoid ESLINT error https://reactjs.org/docs/hooks-faq.html#is-it-safe-to-omit-functions-from-the-list-of-dependencies
+            const {supportedCountries}= MAP_CONSTANTS;          //Need to move into useEffect function to avoid ESLINT error https://reactjs.org/docs/hooks-faq.html#is-it-safe-to-omit-functions-from-the-list-of-dependencies 
             ( async country =>{
                 if(supportedCountries.hasOwnProperty(country)){
+                    setLoading(true);
                     const response = await axios.get(`/api/countries/${supportedCountries[country]}`);
                     setRecipes(response.data);
-                }       
+                    setLoading(false);
+                }
+                setLoading(true);       
                 return;
             })(country);
         },
-        [country]      //empty [] means it works like componentDidMount
+        [country,setLoading]      //empty [] means it works like componentDidMount
     );
     return recipes;
 }
