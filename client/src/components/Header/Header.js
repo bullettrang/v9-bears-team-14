4e9968948087a9864e3ad8c30eb931b9e3d50e1a
Brian <trangb@oregonstate.edu>
2019-06-27 23:09:15 -0700

    animation for header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {useTransition, animated} from 'react-spring';
 import './Header.css';
 const Header =(props)=>{
+    const [show, set] = useState(false)
+
+    const transitions = useTransition(show, null, {
+        from: {  opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        })
+
+    useEffect(() =>  set(state => !state), [])
     return(
-        <header className="Header__Wrapper">
-            <h1 className="Header--Title">Geo-Foods</h1>
-        </header>
+        // <header className="Header__Wrapper">
+        //     <h1 className="Header--Title">Geo-Foods</h1>
+        // </header>
+
+         transitions.map(({item,key,props})=> <animated.header key={key} style={props} className="Header__Wrapper">
+             <h1 className="Header--Title"> Geo-Foods</h1>
+         </animated.header>)
     )
 }
 
